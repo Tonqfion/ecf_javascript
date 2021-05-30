47277711941821150269105fade3790e842070f9
Bertrand Bouland <bbformationcefim@gmail.com>
2021-05-30 23:03:54 +0200

    Result count updated

--- public/js/config.js
+++ public/js/config.js
@@ -4,3 +4,5 @@ export const SEARCH_FIELD_VALUE = document.getElementById("search-input");
 export const SEARCH_BUTTON = document.getElementById("search-btn");
 export const PARENT_ELEMENT = document.getElementById("results-grid");
 export const SEARCH_FILTER = document.getElementById("search-filter");
+export const RESULT_MESSAGE = document.getElementById("result-message");
+export const RESULT_COUNT_MESSAGE = document.getElementById("result-count");
